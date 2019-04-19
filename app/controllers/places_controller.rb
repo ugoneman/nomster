12220321361e093ef18e15236103a91895d35ed7
Ugo Otuonye <ugoneman@gmail.com>
2019-04-19 05:03:10 +0000

    Initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,11 +18,14 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
-
 
 end
