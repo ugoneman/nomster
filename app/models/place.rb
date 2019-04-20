69be1b736edf4e0e2a94e262fe282799fae43d00
Ugo Otuonye <ugoneman@gmail.com>
2019-04-20 05:57:44 +0000

    Initial commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, :address, :description, presence: true, length: { minimum: 3 }
   #validates :name, :address, :description, length: { minimum: 3 }
 end
