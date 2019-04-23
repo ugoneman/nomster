3bdc9733922bd3186d95f27f66019f6108388cdb
Ugo Otuonye <ugoneman@gmail.com>
2019-04-23 04:52:37 +0000

    Initial commit

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,8 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
 
+  belongs_to :photo
+
   geocoded_by :address
   after_validation :geocode
   
---
+++ db/migrate/20190423031732_create_photos.rb
@@ -0,0 +1,12 @@
+class CreatePhotos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :photos do |t|
+      t.text :caption
+      t.integer :place_id
+      t.timestamps
+    end
+
+    add_index :photos, [:caption, :place_id]
+    add_index :photos, :caption
+  end
+end
