3d5c84c70f29c97082edd7c4eccd89ef61a3ada9
Ugo Otuonye <ugoneman@gmail.com>
2019-04-23 21:45:39 +0000

    image upload

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,13 +3,13 @@ class PhotosController < ApplicationController
 
   def create
     @place = Place.find(params[:place_id])
-    photo = @place.photos.create(photo_params)
+    @photo = @place.photos.create(photo_params)
     redirect_to place_path(@place)
   end
 
   private
 
   def photo_params
-    params.require(:photo).permit(:caption)
+    params.require(:photo).permit(:picture)
   end
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,11 +2,12 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
 
-  belongs_to :photo
+  has_many :photos
 
   geocoded_by :address
   after_validation :geocode
   
-  validates :name, :address, :description, presence: true, length: { minimum: 3 }
-  #validates :name, :address, :description, length: { minimum: 3 }
+  validates :name, :address, :description, presence: true
+  validates :name, length: { minimum: 3 }
+  
 end
